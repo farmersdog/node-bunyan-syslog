636efaa4907bf46802d35ea58b6acd62b2b495bb
Behcet <behcet@dolm.us>
2018-04-24 18:55:02 -0400

    RFC 5424 compatible output. options.data and options.tls support

--- test/sys.test.js
+++ test/sys.test.js
@@ -5,77 +5,66 @@ var test = require('tap').test;
 
 var bsyslog = require('../lib');
 
-
-
-///--- Globals
+/// --- Globals
 
 var I = 0;
 var LOG;
 var STREAM;
 
-
-
-///--- Tests
+/// --- Tests
 
 test('create a logger', function (t) {
-        STREAM = bsyslog.createBunyanStream({
-                name: 'sys_test',
-                facility: bsyslog.local0,
-                type: 'sys'
-        });
-        t.ok(STREAM);
-        console.error(STREAM.toString());
+  STREAM = bsyslog.createBunyanStream({
+    name: 'sys_test',
+    facility: bsyslog.local0,
+    type: 'sys'
+  });
+  t.ok(STREAM);
+  console.error(STREAM.toString());
 
-        LOG = bunyan.createLogger({
-                name: 'systest',
-                streams: [ {
-                        type: 'raw',
-                        level: 'trace',
-                        stream: STREAM
-                } ]
-        });
-        t.ok(LOG);
-        STREAM.once('connect', t.end.bind(t));
+  LOG = bunyan.createLogger({
+    name: 'systest',
+    streams: [ {
+      type: 'raw',
+      level: 'trace',
+      stream: STREAM
+    } ]
+  });
+  t.ok(LOG);
+  STREAM.once('connect', t.end.bind(t));
 });
-
 
 test('write a trace record', function (t) {
-        LOG.trace({i: I++}, 'sample %s record', 'trace');
-        t.end();
+  LOG.trace({i: I++}, 'sample %s record', 'trace');
+  t.end();
 });
-
 
 test('write a debug record', function (t) {
-        LOG.debug({i: I++}, 'sample %s record', 'debug');
-        t.end();
+  LOG.debug({i: I++}, 'sample %s record', 'debug');
+  t.end();
 });
-
 
 test('write a info record', function (t) {
-        LOG.info({i: I++}, 'sample %s record', 'info');
-        t.end();
+  LOG.info({i: I++}, 'sample %s record', 'info');
+  t.end();
 });
-
 
 test('write a warn record', function (t) {
-        LOG.warn({i: I++}, 'sample %s record', 'warn');
-        t.end();
+  LOG.warn({i: I++}, 'sample %s record', 'warn');
+  t.end();
 });
-
 
 test('write a error record', function (t) {
-        LOG.error({i: I++}, 'sample %s record', 'error');
-        t.end();
+  LOG.error({i: I++}, 'sample %s record', 'error');
+  t.end();
 });
-
 
 test('write a fatal record', function (t) {
-        LOG.fatal({i: I++}, 'sample %s record', 'fatal');
-        t.end();
+  LOG.fatal({i: I++}, 'sample %s record', 'fatal');
+  t.end();
 });
 
-
 test('teardown', function (t) {
-        STREAM.close();
-        t.end();
+  STREAM.close();
+  t.end();
 });
