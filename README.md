491bf765e01f3821dd82d0d433526ba507e88a35
Behcet <behcet@users.noreply.github.com>
2018-08-21 12:05:43 -0400

    Remove bindings (#1)
    * RFC 5424 compatible output. options.data and options.tls support
    
    * Upgrade nan
    
    * Upgrade nan version
    
    Bumped the nan version to support node.js 6.x versions.  Closes Issues #39
    
    * Using socket.unref()
    
    * Removed native bindings
    
    * Upgrade tap 11 -> 12
    
    * Strict mode
    
    * Removed npmignore in favor of package.json files key
    
    * Updated package name and repo
    
    * Updated readme
    
    * Added keywords to package.json

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 bunyan-syslog is a stream for [bunyan](https://github.com/trentm/node-bunyan)
-that consumes `raw` records from bunyan and sends them to a syslog server.
+that consumes `raw` records from bunyan and sends them to a remote syslog server.
 
 # Installation
 
-    npm install bunyan-syslog
+    npm install -S @farmersdog/bunyan-syslog
+
+# About
+
+This fork is compliant with [RFC 5424](https://tools.ietf.org/html/rfc5424). It
+supports Structured Data component in messages as well as TLS over TCP.
+
+Local syslog bindings have been removed.
 
 # Usage
 
@@ -12,26 +19,31 @@ var bunyan = require('bunyan');
 var bsyslog = require('bunyan-syslog');
 
 var log = bunyan.createLogger({
-	name: 'foo',
-	streams: [ {
-		level: 'debug',
-		type: 'raw',
-		stream: bsyslog.createBunyanStream({
-			type: 'sys',
-			facility: bsyslog.local0,
-			host: '192.168.0.1',
-			port: 514
-		})
-	}]
+  name: 'foo',
+  streams: [{
+    level: 'debug',
+    type: 'raw',
+    stream: bsyslog.createBunyanStream({
+      type: 'tcp',
+      tls: true,
+      data: '[structured data]'
+      facility: bsyslog.local0,
+      host: '192.168.0.1',
+      port: 514
+    })
+  }]
 });
 
-log.debug({foo: 'bar'}, 'hello %s', 'world');
+log.debug({ foo: 'bar' }, 'hello %s', 'world');
 ```
-That's pretty much it.  You create a syslog stream, and point it at a syslog
-server (UDP by default; you can force TCP by setting `type: tcp` in the
-constructor); default is to use facility `user` and a syslog server on
-`127.0.0.1:514`.  Note you *must* pass `type: 'raw'` to bunyan in the top-level
+
+That's pretty much it. You create a syslog stream, and point it at a syslog
+server (UDP by default; you can use TCP by setting `type: tcp` in the
+constructor).
+
+Note you *must* pass `type: 'raw'` to bunyan in the top-level
 stream object or this won't work.
+
 
 ## Mappings
 
--- test/tcp.test.js
+++ test/tcp.test.js
@@ -1,81 +1,96 @@
 // Copyright 2013 Mark Cavage, Inc.  All rights reserved.
 
+'use strict';
+
+var net = require('net');
 var bunyan = require('bunyan');
 var test = require('tap').test;
 
 var bsyslog = require('../lib');
 
-
-
-///--- Globals
-
 var I = 0;
 var LOG;
 var STREAM;
+var HOST = process.env.LOG_HOST;
+var PORT = parseInt(process.env.TCP_LOG_PORT, 10) || 10514;
+var SERVER;
 
-
-
-///--- Tests
+test('setup', t => {
+  if (!HOST) {
+    SERVER = net.createServer(c => {
+      c.on('data', data => (
+        SERVER._messages = SERVER._messages.concat(data.toString().split('\n')))
+      );
+    });
+    SERVER._messages = [];
+    SERVER.listen(PORT);
+  }
+  t.end();
+});
 
 test('create a logger', function (t) {
-        STREAM = bsyslog.createBunyanStream({
-                host: process.env.LOG_HOST,
-                port: parseInt(process.env.TCP_LOG_PORT || 10514, 10),
-                facility: bsyslog.facility.local0,
-                type: 'tcp'
-        });
-        t.ok(STREAM);
+  STREAM = bsyslog.createBunyanStream({
+    host: HOST,
+    port: PORT,
+    facility: bsyslog.facility.local0,
+    type: 'tcp'
+  });
+  t.ok(STREAM);
+  console.error(STREAM.toString());
+  STREAM.once('connect', t.end.bind(t));
 
-        LOG = bunyan.createLogger({
-                name: 'tcptest',
-                streams: [ {
-                        type: 'raw',
-                        level: 'trace',
-                        stream: STREAM
-                } ]
-        });
-        t.ok(LOG);
-        STREAM.once('connect', t.end.bind(t));
+  LOG = bunyan.createLogger({
+    name: 'tcptest',
+    streams: [{
+      type: 'raw',
+      level: 'trace',
+      stream: STREAM
+    }]
+  });
+  t.ok(LOG);
 });
-
 
 test('write a trace record', function (t) {
-        LOG.trace({i: I++}, 'sample %s record', 'trace');
-        t.end();
+  LOG.trace({i: I++}, 'sample %s record', 'trace');
+  t.end();
 });
-
 
 test('write a debug record', function (t) {
-        LOG.debug({i: I++}, 'sample %s record', 'debug');
-        t.end();
+  LOG.debug({i: I++}, 'sample %s record', 'debug');
+  t.end();
 });
-
 
 test('write a info record', function (t) {
-        LOG.info({i: I++}, 'sample %s record', 'info');
-        t.end();
+  LOG.info({i: I++}, 'sample %s record', 'info');
+  t.end();
 });
-
 
 test('write a warn record', function (t) {
-        LOG.warn({i: I++}, 'sample %s record', 'warn');
-        t.end();
+  LOG.warn({i: I++}, 'sample %s record', 'warn');
+  t.end();
 });
-
 
 test('write a error record', function (t) {
-        LOG.error({i: I++}, 'sample %s record', 'error');
-        t.end();
+  LOG.error({i: I++}, 'sample %s record', 'error');
+  t.end();
 });
-
 
 test('write a fatal record', function (t) {
-        LOG.fatal({i: I++}, 'sample %s record', 'fatal');
-        t.end();
+  LOG.fatal({i: I++}, 'sample %s record', 'fatal');
+  t.end();
 });
 
-
 test('teardown', function (t) {
-        STREAM.close();
-        t.end();
+  STREAM.close();
+  SERVER.close(server => {
+    // TODO better way to parse syslog messages
+    var i = Math.max(...SERVER._messages
+      .map(m => m.substr(m.indexOf('{'), m.length))
+      .filter(Boolean)
+      .map(m => JSON.parse(m))
+      .map(m => m.i)
+    );
+    t.equals(i, --I);
+    t.end();
+  });
 });
